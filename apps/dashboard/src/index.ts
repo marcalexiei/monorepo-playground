f367c4cc8a6b1e255aec741587a9df840922b988
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-01-14 12:29:55 +0100

    refactor: add biome as formatter

--- apps/dashboard/src/index.ts
+++ apps/dashboard/src/index.ts
@@ -2,10 +2,10 @@ import { UIBox } from "@monorepoplayground/components";
 import { User } from "@monorepoplayground/models";
 
 export function createDashboard(): string {
-const user: User = {
-  name: 'funny',
-  surname: 'things'
-};
+  const user: User = {
+    name: "funny",
+    surname: "things",
+  };
 
   return `dashboard ${UIBox(user.name)}`;
 }
